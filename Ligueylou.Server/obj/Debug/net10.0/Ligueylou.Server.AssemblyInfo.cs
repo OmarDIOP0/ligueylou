37b08689b5ecf8d8a7b495e263bc22ed07227c79
OmarDIOP0 <odiop6170@gmail.com>
2025-12-04 23:48:30 +0000

    implementation de la classe reservation

--- Ligueylou.Server/obj/Debug/net10.0/Ligueylou.Server.AssemblyInfo.cs
+++ Ligueylou.Server/obj/Debug/net10.0/Ligueylou.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ligueylou.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e9963af20158ef551a0747d1b2afb02fd940c9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1c4f6f664e39330eb3125244751f32c08de67fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ligueylou.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ligueylou.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
