de76a2eb380cc771eefb8cadd52706db99c7d870
OmarDIOP0 <odiop6170@gmail.com>
2025-12-04 22:43:17 +0000

    creation de la classe enum

--- Ligueylou.Server/obj/Debug/net10.0/Ligueylou.Server.AssemblyInfo.cs
+++ Ligueylou.Server/obj/Debug/net10.0/Ligueylou.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ligueylou.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b40646e0cf2d97570738c7aaf6dfa12a15deffd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5870549ba9653ac3705e867c94a55b8a3beeefa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ligueylou.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ligueylou.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
