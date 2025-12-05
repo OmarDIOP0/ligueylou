ace42c1665974685f8fda7ba0f088521cec57f28
OmarDIOP0 <odiop6170@gmail.com>
2025-12-05 22:50:09 +0000

    modification de la classe Service

--- Ligueylou.Server/Models/Service.cs
+++ Ligueylou.Server/Models/Service.cs
@@ -10,16 +10,17 @@ namespace Ligueylou.Server.Models
         public Guid Id { get; set; } = Guid.NewGuid();
         public TypeServiceEnum TypeService { get; set; }
         public int? Duree { get; set; }
-        public double tarifHoraire { get; set; }
+        public double TarifHoraire { get; set; }
         public string? Description { get; set; }
         public Guid PrestataireId { get; set; }
         public virtual Prestataire Prestataire { get; set; }
-        public ICollection<Evaluation> Evaluations { get; set; }
-        public Guid ReservationId { get; set; }
+
+        public Guid? ReservationId { get; set; }
         public virtual Reservation Reservation { get; set; }
         public DateTime DateCreation { get; set; } = DateTime.Now;
-        public ICollection<Notification> Notifications { get; set; }
-        public ICollection<Paiement> Paiements { get; set; }
+        public ICollection<Notification> Notifications { get; set; } = new HashSet<Notification>();
+        public ICollection<Paiement> Paiements { get; set; } = new HashSet<Paiement>();
+        public ICollection<Evaluation> Evaluations { get; set; } = new HashSet<Evaluation>();
     }
 }
 
