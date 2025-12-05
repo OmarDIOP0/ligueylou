82fd69f4abbe3861ab1a7569a86f63627f342d1a
OmarDIOP0 <odiop6170@gmail.com>
2025-12-05 22:44:12 +0000

    modification de la classe Admin

--- Ligueylou.Server/Models/Administrateur.cs
+++ Ligueylou.Server/Models/Administrateur.cs
@@ -1,8 +1,12 @@
 ﻿using Ligueylou.Server.Models.abstracts;
+using System.ComponentModel.DataAnnotations;
 namespace Ligueylou.Server.Models
 {
     public class Administrateur : Utilisateur
     {
+        [Required]
         public string CodeSecret { get; set; }
+        public DateTime DateCreation { get; set; } = DateTime.Now;
+        public DateTime? DerniereConnexion { get; set; }
     }
 }
