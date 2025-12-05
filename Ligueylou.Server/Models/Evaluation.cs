6f2c3ad018be103f565a4ad097ea34d4a6ca2dc7
OmarDIOP0 <odiop6170@gmail.com>
2025-12-05 22:53:54 +0000

    modification de la classe Evaluation

--- Ligueylou.Server/Models/Evaluation.cs
+++ Ligueylou.Server/Models/Evaluation.cs
@@ -7,11 +7,13 @@ namespace Ligueylou.Server.Models
         [Key]
         public Guid Id { get; set; } = Guid.NewGuid();
         [Range(0, 5, ErrorMessage = "Le score doit être compris entre 0 et 5.")]
-        public double Score { get; set; }
+        public double Score { get; set; } = 0;
         public string? Commentaire { get; set; }
         public Guid ClientId { get; set; }
-        public virtual Client Client { get; set; }
+        public DateTime DateCreation { get; set; } = DateTime.Now;
+
         public Guid ServiceId { get; set; }
+        public virtual Client Client { get; set; }
         public virtual Service Service { get; set; }
     }
 }
