23325886fd9c499aacf515f5dd8180db51f67a98
OmarDIOP0 <odiop6170@gmail.com>
2025-12-07 16:58:12 +0000

    utilisateur controlleur

---
+++ Ligueylou.Server/Controllers/UtilisateurController.cs
@@ -0,0 +1,47 @@
+﻿using Ligueylou.Server.Request;
+using Ligueylou.Server.Services.Utilisateurs;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Ligueylou.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UtilisateursController : ControllerBase
+    {
+        private readonly IUtilisateurService _service;
+
+        public UtilisateursController(IUtilisateurService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var user = await _service.GetUtilisateurById(id);
+            return user == null ? NotFound() : Ok(user);
+        }
+
+        [HttpGet("email/{email}")]
+        public async Task<IActionResult> GetByEmail(string email)
+        {
+            var user = await _service.GetUtilisateurByEmail(email);
+            return user == null ? NotFound() : Ok(user);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _service.GetAllUtilisateurs());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUtilisateurDto dto)
+        {
+            var created = await _service.AddUtilisateur(dto);
+            return CreatedAtAction(nameof(Get), new { id = created.Id }, created);
+        }
+    }
+
+}
--- Ligueylou.Server/obj/Debug/net10.0/Ligueylou.Server.AssemblyInfo.cs
+++ Ligueylou.Server/obj/Debug/net10.0/Ligueylou.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ligueylou.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2491ce16ecf89cb052f5f6a50301168ddbc24e22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77c29128bb3e959e2933be365ced49ec4b590d5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ligueylou.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ligueylou.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
