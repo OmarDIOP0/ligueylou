5661d093fcc7cf3fc6d2339d87bbd6bae7fdffc0
OmarDIOP0 <odiop6170@gmail.com>
2025-12-09 21:27:26 +0000

    implementation de prestataire interface

---
+++ Ligueylou.Server/Services/IPrestataireService.cs
@@ -0,0 +1,28 @@
+﻿namespace Ligueylou.Server.Services
+{
+    public interface IPrestataireService
+    {
+        Task<PrestataireDto> AddPrestataire(Prestataire prestataire);
+        Task<PrestataireDto?> GetPrestataireById(Guid id);
+        Task<IEnumerable<PrestataireDto>> GetAllPrestataires();
+        Task<PrestataireDto?> UpdatePrestataire(Guid id, Prestataire updated);
+        Task<bool> DeletePrestataire(Guid id);
+
+        Task<PrestataireDto?> UpdateAdresse(Guid id, Adresse adresse);
+        Task<PrestataireDto?> AddSpecialite(Guid id, Specialite specialite);
+        Task<PrestataireDto?> AddReservation(Guid id, Reservation reservation);
+
+        Task<bool> IsActif(Guid id);
+        Task<PrestataireDto?> ActivatePrestataire(Guid id);
+
+        Task<double?> GetScore(Guid id);
+        Task<PrestataireDto?> UpdateScore(Guid id, double score);
+
+        Task<IEnumerable<PrestataireDto>> SearchByName(string name);
+        Task<IEnumerable<PrestataireDto>> Filter(string? ville, string? specialite, bool? actif);
+        Task<IEnumerable<PrestataireDto>> GetTopRated(int topN);
+
+        Task<IEnumerable<PrestataireDto>> GetPaged(int page, int size);
+    }
+
+}
--- Ligueylou.Server/obj/Debug/net10.0/Ligueylou.Server.AssemblyInfo.cs
+++ Ligueylou.Server/obj/Debug/net10.0/Ligueylou.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ligueylou.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ffe9e6975ce7a26298bea73499f3712219bf257")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79d1b453ddcc2336d80ecc8a36a4f0401eb840e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ligueylou.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ligueylou.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
