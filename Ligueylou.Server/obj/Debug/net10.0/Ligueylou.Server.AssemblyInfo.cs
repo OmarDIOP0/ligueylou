4530fc838c3e907801c0e03dc3e888d43ba0b688
OmarDIOP0 <odiop6170@gmail.com>
2025-12-10 17:01:06 +0000

    integration de daisyui

--- Ligueylou.Server/obj/Debug/net10.0/Ligueylou.Server.AssemblyInfo.cs
+++ Ligueylou.Server/obj/Debug/net10.0/Ligueylou.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ligueylou.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4869b6023a8de188e35954d1590db03f6cab8b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38132c4b1a577203223cef55bb5601a261861db3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ligueylou.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ligueylou.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ligueylou.client/tailwind.config.js
+++ ligueylou.client/tailwind.config.js
@@ -1,16 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('daisyui'), 
-  ],
-  daisyui: {
-    themes: ["light", "dark"],
-    base: true,
-  },
-};
+///** @type {import('tailwindcss').Config} */
+//module.exports = {
+//  content: [
+//    "./src/**/*.{html,ts}",
+//  ],
+//  theme: {
+//    extend: {},
+//  },
+//  plugins: [
+//    require('daisyui'), 
+//  ],
+//  daisyui: {
+//    themes: ["light", "dark"],
+//    base: true,
+//  },
+//};
