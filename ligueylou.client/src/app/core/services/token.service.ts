69a36ba33cda9e2fa4f136b3b688f425f0abccfe
OmarDIOP0 <odiop6170@gmail.com>
2025-12-12 01:06:48 +0000

    implementation du guard

--- ligueylou.client/src/app/core/services/token.service.ts
+++ ligueylou.client/src/app/core/services/token.service.ts
@@ -9,23 +9,30 @@ export class TokenService {
 
   private readonly TOKEN_KEY = 'access_token';
   private readonly REFRESH_TOKEN_KEY = 'refresh_token';
+  private readonly EXPIRED_TOKEN_KEY = 'expired_token';
 
   private _token = signal<string | null>(localStorage.getItem(this.TOKEN_KEY));
   private _refreshToken = signal<string | null>(localStorage.getItem(this.REFRESH_TOKEN_KEY));
+  private _expiredToken = signal<string | null>(localStorage.getItem(this.EXPIRED_TOKEN_KEY));
+
 
   token = this._token.asReadonly();
   refreshToken = this._refreshToken.asReadonly();
+  expiredToken = this._expiredToken.asReadonly();
+  
 
   getToken(): string | null {
     return localStorage.getItem(this.TOKEN_KEY);
   }
 
-  setToken(token: string, refresh: string) {
+  setToken(token: string, refresh: string, expired?:number | null) {
     this._token.set(token);
     this._refreshToken.set(refresh);
+    //this._expiredToken.set(expired);
 
     localStorage.setItem(this.TOKEN_KEY, token);
     localStorage.setItem(this.REFRESH_TOKEN_KEY, refresh);
+    //if (expired) sessionStorage.setItem('access_exp', expired.toString());
   }
 
   clearToken() {
