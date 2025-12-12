2d70c005a1f6c6f0c4040a32debe217ca113e8c8
OmarDIOP0 <odiop6170@gmail.com>
2025-12-12 21:13:14 +0000

    client

--- ligueylou.client/src/app/features/client/client.module.ts
+++ ligueylou.client/src/app/features/client/client.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClientComponent } from './components/client/client.component';
+import { ClientDashboardComponent } from './components/client-dashboard/client-dashboard.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ClientComponent,
+    ClientDashboardComponent
+  ],
   imports: [
     CommonModule
   ]
