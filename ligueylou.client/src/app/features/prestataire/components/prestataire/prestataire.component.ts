8dbe4f8eded31be06c81c4ddd49524e3df98e750
OmarDIOP0 <odiop6170@gmail.com>
2025-12-12 21:09:13 +0000

    route du prestataire

---
+++ ligueylou.client/src/app/features/prestataire/components/prestataire/prestataire.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-prestataire',
+  standalone: true,
+  import: [RouterModule],
+  templateUrl: './prestataire.component.html',
+  styleUrl: './prestataire.component.css'
+})
+export class PrestataireComponent {
+
+}
