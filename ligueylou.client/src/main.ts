907fffff4d6f1b77896cac0aae69bae29ff260d7
OmarDIOP0 <odiop6170@gmail.com>
2025-12-12 00:24:02 +0000

    injection de l'intercepteur

--- ligueylou.client/src/main.ts
+++ ligueylou.client/src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorInterceptor, jwtInterceptor } from './app/core/interceptors/jwt.interceptor';
 
 bootstrapApplication(AppComponent, {
