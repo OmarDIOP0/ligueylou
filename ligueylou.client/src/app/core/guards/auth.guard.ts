f5f299a583e21c9ce43247fd605a0bc985d55306
OmarDIOP0 <odiop6170@gmail.com>
2025-12-12 00:56:09 +0000

    implementation du guard

---
+++ ligueylou.client/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,12 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { TokenService } from '../services/token.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const tokenService = inject(TokenService);
+  const router = inject(Router);
+
+  if (tokenService.token())return true;
+
+  return router.createUrlTree(['/login']);
+};
