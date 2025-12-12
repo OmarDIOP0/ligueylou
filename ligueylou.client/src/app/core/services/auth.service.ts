d499c186c82b1c5a9b5eec9bfbfb1cc95dd192ff
OmarDIOP0 <odiop6170@gmail.com>
2025-12-12 00:04:13 +0000

    implementation login et register

--- ligueylou.client/src/app/core/services/auth.service.ts
+++ ligueylou.client/src/app/core/services/auth.service.ts
@@ -1,6 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { TokenService } from './token.service';
+import { ApiEndpointsService } from '../config/api-endpoints.service';
+import { catchError, throwError } from 'rxjs';
+import { RegisterRequestDto } from '../models/RegisterRequestDto';
+import { LoginRequestDto } from '../models/LoginRequestDto';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +12,28 @@ import { TokenService } from './token.service';
 export class AuthService {
   private http = inject(HttpClient);
   private tokenService = inject(TokenService);
+  private endPoints = inject(ApiEndpointsService);
+  private AUTH_ENDPOINTS = this.endPoints.auth;
+
+  register(payload: RegisterRequestDto) {
+    return this.http.post(this.AUTH_ENDPOINTS.register, payload)
+      .pipe(
+        catchError(err => {
+          console.error('Erreur API register', err);
+          return throwError(() => new Error('Impossible de créer un compte avec ces informations.'));
+        }
+        ));
+  }
+
+  login(payload: LoginRequestDto) {
+    return this.http.post(this.AUTH_ENDPOINTS.login, payload)
+      .pipe(
+        catchError(err => {
+          console.error('Erreur API login', err);
+          return throwError(() => new Error('Impossible de se connecter avec ces informations.'));
+        }
+        ));
+  }
+
 
 }
