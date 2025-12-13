311475c1fda6c3871b6bd21490c72a15ee1074cf
OmarDIOP0 <odiop6170@gmail.com>
2025-12-13 17:21:49 +0000

    ajout du auth admin

--- ligueylou.client/src/app/features/admin/admin.module.ts
+++ ligueylou.client/src/app/features/admin/admin.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 
 
 
 @NgModule({
   declarations: [
     AdminDashboardComponent,
-    AdminComponent
+    AdminComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule
