c911abab7037358656817fab76bd13ba0ba1cb6e
OmarDIOP0 <odiop6170@gmail.com>
2025-12-13 19:44:59 +0000

    modification

--- ligueylou.client/src/app/features/admin/admin.module.ts
+++ ligueylou.client/src/app/features/admin/admin.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { CustomizerPanelComponent } from './utils/customizer-panel/customizer-panel.component';
 import { VerticalHeaderComponent } from './utils/vertical-header/vertical-header.component';
+import { VerticalSidebarComponent } from './utils/vertical-sidebar/vertical-sidebar.component';
 
 
 
@@ -16,7 +17,8 @@ import { VerticalHeaderComponent } from './utils/vertical-header/vertical-header
     LoginComponent,
     RegisterComponent,
     CustomizerPanelComponent,
-    VerticalHeaderComponent
+    VerticalHeaderComponent,
+    VerticalSidebarComponent
   ],
   imports: [
     CommonModule
