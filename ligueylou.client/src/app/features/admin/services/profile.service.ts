9d5e2ddfe858fdbdd242c961f30cfa262e8b44ca
OmarDIOP0 <odiop6170@gmail.com>
2025-12-16 23:18:54 +0000

    ajout de l'interface modification

--- ligueylou.client/src/app/features/admin/services/profile.service.ts
+++ ligueylou.client/src/app/features/admin/services/profile.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { TokenService } from '../../../core/services/token.service';
+import { ApiEndpointsService } from '../../../core/config/api-endpoints.service';
+import { Observable } from 'rxjs';
+import { UpdateUtilisateurDto, UtilisateurDto } from '../../../core/models/UtilisateurDto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private http = inject(HttpClient);
+  private tokenService = inject(TokenService);
+  private endPoints = inject(ApiEndpointsService);
 
-  constructor() { }
+  getProfile(): Observable<UtilisateurDto> {
+    return this.http.get<UtilisateurDto>(`${this.endPoints.utilisateur}/profile`);
+  }
+  updateProfile(id: string, payload: UpdateUtilisateurDto): Observable<UtilisateurDto> {
+    return this.http.put<UtilisateurDto>(`${this.endPoints.utilisateur}/${id}`, payload);
+  }
+
+  deleteAccount(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.endPoints.utilisateur}/${id}`);
+  }
+
+  logout(): Observable<any> {
+    return this.http.post(`${this.endPoints.utilisateur}/logout`, {});
+  }
 }
