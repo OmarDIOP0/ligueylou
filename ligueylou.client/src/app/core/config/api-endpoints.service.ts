ea8eaa2f6aa713c0e65f9d30946c9bd4de0bf679
OmarDIOP0 <odiop6170@gmail.com>
2025-12-17 23:22:09 +0000

    implementation des endpoints du client

--- ligueylou.client/src/app/core/config/api-endpoints.service.ts
+++ ligueylou.client/src/app/core/config/api-endpoints.service.ts
@@ -19,5 +19,20 @@ export class ApiEndpointsService {
     profile: `${this.base}/Utilisateurs/profile`,
     logout: `${this.base}/Utilisateurs/logout`,
   };
-
+  client = {
+    create: `${this.base}/Clients`,
+    getAll: `${this.base}/Clients`,
+    get: (id: string) => `${this.base}/Clients`,
+    getByEmail: (email: string) => `${this.base}/Clients`,
+    getByTelephone: (telephone: string) => `${this.base}/Clients`,
+    update: (id: string) => `${this.base}/Clients`,
+    delete: (id: string) => `${this.base}/Clients`,
+    updateAdresse: (id: string) => `${this.base}/Clients/${id}/adresse`,
+    createFavori: (clientId: string) => `${this.base}/Clients/${clientId}/favoris`,
+    getFavoris: (clientId: string) => `${this.base}/Clients/${clientId}/favoris`,
+    deleteFavori: (clientId: string, favoriId: string) => `${this.base}/Clients/${clientId}/favoris/${favoriId}`,
+    getReservations: (clientId: string) => `${this.base}/Clients/${clientId}/reservations`,
+    getEvaluations: (clientId: string) => `${this.base}/Clients/${clientId}/evaluations`,
+    getFilter: (actif: boolean, email: string, telephone: string) => `${this.base}/Clients/filter`,
+  }
 }
