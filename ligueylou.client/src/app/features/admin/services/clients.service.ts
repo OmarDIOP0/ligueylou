badf268fd764f5b2b9b543a30f10bef5e2bdfbb0
OmarDIOP0 <odiop6170@gmail.com>
2025-12-18 01:07:36 +0000

    modification

--- ligueylou.client/src/app/features/admin/services/clients.service.ts
+++ ligueylou.client/src/app/features/admin/services/clients.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { TokenService } from '../../../core/services/token.service';
+import { ApiEndpointsService } from '../../../core/config/api-endpoints.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientsService {
-
-  constructor() { }
+  private http = inject(HttpClient);
+  private tokenService = inject(TokenService);
+  private endPoints = inject(ApiEndpointsService);
 }
